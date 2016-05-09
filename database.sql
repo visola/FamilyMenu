33d27b7277c922d0351ec11c8a1a4bae259acb8a
Vinicius Isola <viniciusisola@gmail.com>
2016-05-09 16:28:16 -0400

    Add endpoint to fetch all items

---
+++ database.sql
@@ -0,0 +1,7 @@
+create table items (
+  id int not null auto_increment,
+  day date,
+  meal varchar(30),
+  item varchar(255),
+  primary key (id)
+);
--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-var express = require("express");
+var express = require('express');
+var mysql = require('mysql');
 var app = express();
+var connection;
+
+if (process.env.DATABASE_URL) {
+  connection = mysql.createConnection(process.env.DATABASE_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12345678',
+    database: 'familyMenu'
+  });
+}
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
+function getKey(date) {
+    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+}
+
+app.get('/items', function (req, resp) {
+  connection.connect();
+  connection.query('select * from items', function (err, rows, fields) {
+    var result = {};
+    rows.forEach(function (row, index) {
+      var day = result[getKey(row.day)];
+      if (!day) {
+        day = {LUNCH: [], DINNER: []};
+        result[getKey(row.day)] = day;
+      }
+
+      day[row.meal].push(row.item);
+    });
+
+    resp.json(result);
+  });
+  connection.end();
+});
 
 app.listen(app.get('port'), function() {
   console.log('Server is running on port', app.get('port'));
